033e14098e6d4dba3bd90a4108800c841487411c
bconclub <bconclubx@gmail.com>
2026-05-08 17:00:31 +0530

    IntroPage: swap team doctor photos for patient testimonial avatars in social proof row

--- src/assessment-app/pages/IntroPage/IntroPage.tsx
+++ src/assessment-app/pages/IntroPage/IntroPage.tsx
@@ -15,11 +15,11 @@ type IntroPageProps = {
    (royalty-free photos, hot-link friendly). NOT team members.
    To swap any specific face, change the {gender}/{0-99}.jpg index. */
 const AVATARS = [
-  "/images/team/priya-pal.jpg",
-  "/images/team/karthik-ravi.jpg",
-  "/images/team/gayatri-rajamani.jpg",
-  "/images/team/reshmi-sinha.jpg",
-  "/images/team/namratha-nataraj.jpg",
+  "/testimonials/nithya.jpg",
+  "/testimonials/thamarai.jpg",
+  "/testimonials/kat.jpg",
+  "/images/team/ashwini-saras.jpg",
+  "/images/team/shakeela.jpg",
 ];
 
 export function IntroPage({ onStart, onContinue, savedStep, savedTotal, savedScreen }: IntroPageProps) {
